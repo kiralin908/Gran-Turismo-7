ba1a1c5a3f57627b94c52228461ee632f0dcc17e
kiralin <highfoot908@livemail.tw>
2023-11-22 17:01:24 +0800

    Initial Commit

---
+++ Gran Turismo 7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gran Turismo 7
+//
+//  Created by 林郁琦 on 2023/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
